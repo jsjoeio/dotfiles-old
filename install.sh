ac21000bf20afc9bebeaf69944f6f8483036d0f8
Joe Previte <jjprevite@gmail.com>
2021-01-06 23:22:14 +0000

    fix: rust installation

--- install.sh
+++ install.sh
@@ -23,5 +23,5 @@ echo "Installed jq version $(jq --version)"
 export PKG_CONFIG_PATH=/usr/bin/pkg-config
 
 # install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 

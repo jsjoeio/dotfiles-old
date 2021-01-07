2ba75687c7d7e1b87b2c5ff6c9aa10b7796af331
Joe Previte <jjprevite@gmail.com>
2021-01-07 17:04:05 +0000

    update

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ sudo chsh -s $(which zsh) $USER
 
 # install the zsh-syntax-highlighting plugin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 # install jq
 # helpful for installing vsx extensions

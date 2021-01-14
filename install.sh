0b4f72df1a42711f0a191c1fe896d6e9538d4c78
Joe Previte <jjprevite@gmail.com>
2021-01-14 18:22:04 +0000

    feat: update install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,56 @@
 # I want it to be today in my timezone
 export TZ="/usr/share/zoneinfo/America/Phoenix"
 
-# install zsh and set it up
-echo "Installing zsh..."
-sudo apt install -y zsh
-echo "Installed zsh version $(zsh --version)"
-sudo chsh -s $(which zsh) $USER
+###########################
+# zsh setup
+###########################
+echo -e "⤵ Installing zsh..."
+sudo apt-get -y install zsh
+echo -e "✅ Successfully installed zsh version: $(zsh --version)"
 
+# Set up zsh tools
+PATH_TO_ZSH_DIR=$HOME/.oh-my-zsh
+echo -e "Checking if .oh-my-zsh directory exists at $PATH_TO_ZSH_DIR..."
+if [ -d $PATH_TO_ZSH_DIR ]
+then
+   echo -e "\n$PATH_TO_ZSH_DIR directory exists!\nSkipping installation of zsh tools.\n"
+else
+   echo -e "\n$PATH_TO_ZSH_DIR directory not found."
+   echo -e "⤵ Configuring zsh tools in the $HOME directory..."
+
+   (cd $HOME && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended)
+   echo -e "✅ Successfully installed zsh tools"
+fi
+
+# Set up symlink for .zshrc
+ZSHRC_LINK=$HOME/.zshrc
+if [ -L ${ZSHRC_LINK} ] ; then
+   if [ -e ${ZSHRC_LINK} ] ; then
+      echo -e "\n.zshrc is symlinked corrected"
+   else
+      echo -e "\nOops! Your symlink appears to be broken."
+   fi
+elif [ -e ${ZSHRC_LINK} ] ; then
+   echo -e "\nYour .zshrc exists but is not symlinked."
+   # We have to symlink the .zshrc after we curl the install script
+   # because the default zsh tools installs a new one, even if it finds ours
+   rm $HOME/.zshrc
+   echo -e "⤵ Symlinking your .zshrc file"
+   ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+   echo -e "✅ Successfully symlinked your .zshrc file"
+else
+   echo -e "\nUh-oh! .zshrc missing."
+fi
+
+echo -e "⤵ Changing the default shell"
+sudo chsh -s $(which zsh) $USER
+echo -e "✅ Successfully modified the default shell"
+
+# TODO fix this with new set up
 # install the zsh-syntax-highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+# source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # install jq
 # helpful for installing vsx extensions
@@ -35,3 +75,5 @@ sudo apt-get -y install gnupg
 # Install Golang for code-server
 sudo apt -y install golang-go
 
+# For using NeoVim in VSCode
+sudo apt-get -y install neovim
